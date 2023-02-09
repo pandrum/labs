9c810f840f710204e89d9bc10c55fc57b9b60c25
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2023-02-09 19:08:06 +0100

    lab2

---
+++ lab2/PhoneBook.cs
@@ -0,0 +1,72 @@
+﻿namespace lab2
+{
+    internal class PhoneBook
+    {
+        public List<Person> Persons { get; set; } = new List<Person>();
+
+        public void AddEntry(Person person, Phone phone)
+        {
+            try
+            {
+                foreach (Person p in Persons)
+                {
+                    if (p.Name.Equals(person.Name))
+                    {
+                        p.Phonenumbers.Add(phone);
+                    }
+                }
+                person.Phonenumbers.Add(phone);
+                Persons.Add(person);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unknown name.");
+            }
+        }
+
+        public void AddEntry(Person person)
+        {
+            Persons.Add(person);
+        }
+
+        public void FindPhones(Person person)
+        {
+            try
+            {
+                if (person.Phonenumbers.Count > 1)
+                {
+                    Console.WriteLine($"{person.Name}'s phone numbers are: ");
+                    foreach (var phone in person.Phonenumbers)
+                    {
+                        Console.WriteLine(phone.Number);
+                    }
+                } else
+                {
+                    Console.WriteLine($"{person.Name}'s phone numbers is: {person.Phonenumbers.First().Number}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unknown name.");
+            }
+        }
+
+        public void RemoveEntry(Person person, Phone phone)
+        {
+            try
+            {
+                foreach (var p in Persons)
+                {
+                    if (p.Name.Equals(person.Name))
+                    {
+                        p.Phonenumbers.Remove(phone);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unknown name.");
+            }
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using lab2;
+
+//create first person and new number
+Person p1 = new Person("Peter");
+Person p2 = new Person("Johan");
+Phone phone = new Phone("0705564550");
+
+//create second person and two new numbers
+
+//init phonebook and add new persons
+PhoneBook phonebook = new PhoneBook();
+phonebook.AddEntry(p1, phone);
+phonebook.AddEntry(p2);
+
+foreach (var p in phonebook.Persons)
+{
+    Console.WriteLine(p.Name);
+}
