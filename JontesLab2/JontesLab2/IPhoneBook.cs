9239b434764af3db53f9cec86e2f121d8e616607
JonatanFredriksson <62990124+JonatanFredriksson@users.noreply.github.com>
2023-02-21 17:24:30 +0100

    glömde uppdatera interfacet också
    interface update

--- JontesLab2/JontesLab2/IPhoneBook.cs
+++ JontesLab2/JontesLab2/IPhoneBook.cs
@@ -9,7 +9,7 @@ namespace JontesLab2
     internal interface IPhoneBook
     {
         void RemoveEntry(Phone number);
-        void AddEntry(Person name, List<Phone> value);
+        void AddEntry(Person name, Phone number);
         List<Phone> FindPhones(Person name);
 
     }
