c055fc82ef8e680e6e8bcfca6a8459a30cc6c3ba
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2023-02-25 15:38:12 +0100

    derp

---
+++ lab2/PhoneBookException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class PhoneBookException : Exception
+{
+    public PhoneBookException()
+    {
+    }
+
+    public PhoneBookException(string message)
+        : base(message)
+    {
+    }
+
+    public PhoneBookException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
--- lab2/Program.cs
+++ lab2/Program.cs
@@ -9,20 +9,20 @@ ph1 = new Phone("073656934");
 ph2 = new Phone("019169363");
 ph3 = new Phone("070123456");
 
-//init phonebook and add new persons and numbers
 PhoneBook phonebook = new PhoneBook();
 
 phonebook.AddEntry(p1, ph1);
-phonebook.AddEntry(p2, ph2);
-phonebook.AddEntry(p2, ph3);
+phonebook.AddEntry(p1, ph1);
+//phonebook.AddEntry(p2, ph2);
+//phonebook.AddEntry(p2, ph3);
 
-var numbers = phonebook.FindPhones("Mattias");
+var numbers = phonebook.FindPhones("do not exist");
 foreach (var item in numbers)
 {
     Console.WriteLine(item.Number);
 }
 
-phonebook.RemoveEntry("Mattias", ph1);
+phonebook.RemoveEntry("dadad", ph1);
 
 foreach (var item in numbers)
 {
